2751335564cd113caf304017611625f23cfedfad
Effie Chantzi <EffieChantzi@users.noreply.github.com>
2019-11-25 21:58:05 +0100

    Update BeepSound.m

--- source_code/BeepSound.m
+++ source_code/BeepSound.m
@@ -1,11 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%     Copyright (C) 2019  Efthymia Chantzi      %%
+%%        GNU General Public license v3          %%
+%%                 (LICENSE.md)                  %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%  BeepSound function - 19/10/29  %%
-
-               %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-               %%            Chantzi Effie             %%
-               %%           COMBSecretomics            %%
-               %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This function produces a beeping sound when COMBSecretomics terminates.%      
@@ -23,9 +22,9 @@ function BeepSound(d)
 cf = 2000;                  % carrier frequency (Hz)
 sf = 22050;                 % sample frequency (Hz)
                             % d: duration (s)
-n = sf * d;                 % number of samples
-s = (1:n) / sf;             % sound data preparation
-s = sin(2 * pi * cf * s);   % sinusoidal modulation
+n = sf*d;                 % number of samples
+s = (1 : n)/sf;             % sound data preparation
+s = sin(2*pi*cf*s);   % sinusoidal modulation
 sound(s, sf);               % sound presentation
 pause(d + 0.5);             % waiting for sound end
 
